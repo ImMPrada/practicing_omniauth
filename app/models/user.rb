9aac24947c79be8dc7c8952c18964a777118069d
immprada <im.mprada@gmail.com>
2023-06-21 19:06:05 -0500

    user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_validation :add_avatar_url, on: :create
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +8,11 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: true, presence: true
   validates :avatar_url, presence: true
+
+  private
+
+  def add_avatar_url
+    byebug
+    self.avatar_url = "https://api.dicebear.com/6.x/bottts/svg?seed=#{username}"
+  end
 end
